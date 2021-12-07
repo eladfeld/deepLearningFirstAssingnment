began softmax grad fix

code/NN2.py
@@ -1,20 +1,21 @@
 import numpy as np
 import funcs
-from utils import fs
+from utils import fs, unison_shuffled_copies
 # from matplotlib import pyplot as plt
 # import random
 # from utils import *
 # import scipy.io as sio
 # from copy import deepcopy
-LR = 0.15
+LR = 2
 LR_DECAY = .9999
-MIN_LR = 0.025
+MIN_LR = 0.000000001
+DEBUG = False
 
 class Layer:
     def __init__(self, in_dim, out_dim):
         np.random.seed(42)
-        self.weights = np.random.uniform(size=(in_dim, out_dim), low=-1.0, high=1.0 )
-        self.bias= np.random.uniform(size=(out_dim, 1), low=-1.0, high=1.0 ) #todo: may need to switch dims of size param
+        self.weights = np.random.normal(size=(in_dim, out_dim), loc=0.0, scale=1.0 )
+        self.bias= np.random.normal(size=(out_dim, 1), loc=0.0, scale=1.0 )
         self.Y = np.zeros((out_dim, 1)) #todo: may need to switch dims order
 
 class NN:
@@ -29,76 +30,125 @@ class NN:
             self.layers[i] = Layer(layer_dims[i], layer_dims[i+1])
 
     def predict(self, X):
+        my_print("\n\n********** Predict() **********")
+        my_print(f"input: {X}")
         output = X
 
         #propogate data forward through the layers
         for i in range(0, self.num_layers):
+            my_print(f"\nlayer: {i}")
+            my_print(f"\tW: {self.layers[i].weights}")
             output = (output @ self.layers[i].weights)
+            my_print(f"\toutput: {output}")
             #output += self.layers[i].bias.T
             f = funcs.softmax if (i == self.num_layers - 1) else self.act
+
+            f_str = "softmax" if (i == self.num_layers - 1) else "self.act"
+
             output = f(output)
+            my_print(f"\t\t{f_str}(output): {output}")
             self.layers[i].Y = output
 
         return output        
 
     def learn(self, expected, data):
+        my_print("\n\n********** Learn() **********")
         pred = self.layers[-1].Y
+        my_print(f"input: {data}\n")
+        my_print(f"pred: {pred}\n")
+        my_print(f"real: {expected}\n")
         err = (pred - expected).T
+        
+        for i in range(2, self.num_layers + 2):
+            my_print(f"\ni: {i}")
+            my_print(f"\terr: {err}")
 
-        for i in range(2, self.num_layers + 1):
             d_f = funcs.grad_softmax if (i == 2) else self.d_act
-            a = self.layers[self.num_layers - i].Y.T
-            b = (err.T * d_f(self.layers[self.num_layers - i + 1].Y))#todo: transpose result, not just err?
-            #print(f"a: {a.shape, }b: {b.shape}, meanB0: {np.mean(b, axis=0, keepdims=True).shape}")
+            d_f_str = "grad_softmax" if (i == 2) else "self.d_act"
+            my_print(f"\td_f: {d_f_str}")
+
+            input = self.layers[self.num_layers - i].Y if (i < self.num_layers + 1) else data
+            output = self.layers[self.num_layers - i + 1].Y
+
+            my_print(f"\tinput: {input}\n")
+            my_print(f"\toutput: {output}\n")
+
+            a = input.T
+            b = (err.T * d_f(output))
+
+            my_print(f"\ta: {a.shape}, b: {b.shape}")
             dW = -self.lr * (a @ b)
-            dB = -(self.lr / 10) * np.mean(b, axis=0, keepdims=True)
-            #print(f"learn: a: {a.shape}, b: {b.shape}, dW: {dW.shape}, dB: {dB.shape}")
+            dB = -(self.lr) * np.mean(b, axis=0, keepdims=True)
+
+            my_print(f"\tdW:\n{dW}")
 
             err = self.layers[self.num_layers - i + 1].weights @ err
+
+            my_print(f"\tW before update:\n{self.layers[self.num_layers - i + 1].weights}")
             self.layers[self.num_layers - i + 1].weights += dW
+            my_print(f"\tW after update:\n{self.layers[self.num_layers - i + 1].weights}")
             self.layers[self.num_layers - i + 1].bias += dB.T
 
-        a = data.T
-        b = err.T * self.d_act(self.layers[0].Y)
-        dW = -self.lr * (a @ b)
-        dB = -(self.lr / 10) * np.mean(b, axis=0, keepdims=True)
-        self.layers[0].weights += dW
-        self.layers[0].bias += dB.T
+
         self.lr = max(LR_DECAY * self.lr, MIN_LR)
 
-    def train(self, inputs, labels, mini_batch_size, batch_size):
+    def train(self, inputs, labels, mini_batch_size, batch_size, num_epochs=1):
         batch_err = 0
         num_correct, total = 0, 0
         p_stats = np.zeros((labels.shape[1]))
         r_stats = np.zeros((labels.shape[1]))
-        
-        for i in range(0, len(inputs), mini_batch_size):
-            input = inputs[i:i+mini_batch_size]
-            expected = labels[i:i+mini_batch_size]
-            pred = self.predict(input)
+        epoch_acc = 0
 
-            batch_err += get_error(pred, expected)
-            num_correct_i, total_i = accuracy(pred, expected)
-            num_correct += num_correct_i
-            total += total_i
+        for epoch in range(num_epochs):
+            inputs, labels = unison_shuffled_copies(inputs, labels)
+            print(f"---------- Epoch #{epoch} ----------")
+    
+            for i in range(0, len(inputs), mini_batch_size):
+                input = inputs[i:i+mini_batch_size]
+                expected = labels[i:i+mini_batch_size]
+                pred = self.predict(input)
 
-            p_stats_i, r_stats_i = stats(pred, expected)
-            p_stats += p_stats_i
-            r_stats += r_stats_i
+                batch_err += get_error(pred, expected)
+                num_correct_i, total_i = accuracy(pred, expected)
+                num_correct += num_correct_i
+                total += total_i
+                epoch_acc += num_correct_i
 
-            self.learn(expected, input)
+                p_stats_i, r_stats_i = stats(pred, expected)
+                p_stats += p_stats_i
+                r_stats += r_stats_i
+                    
 
-            if (i + mini_batch_size) % batch_size == 0:
-                # print(f"batch #{int(i/batch_size)}:\twrongs: {bad_preds}/{batch_size}\terr: {batch_err}")
-                # print(f"batch #{int(i/batch_size)}:\terr: {batch_err/batch_size}")
-                print(f"{int(i/batch_size)}\tlr: {fs(self.lr)}\terr: {fs(batch_err/batch_size)}\tacc: {num_correct}/{total}\tps: {p_stats}\trs: {r_stats}")
-                batch_err = 0
-                num_correct, total = 0, 0
-                p_stats = np.zeros((labels.shape[1]))
-                r_stats = np.zeros((labels.shape[1]))
+                self.learn(expected, input)
+
+                if (i + mini_batch_size) % batch_size == 0:
+                    print(f"{int(i/batch_size)}\tlr: {fs(self.lr)}\terr: {fs(batch_err/batch_size)}\tacc: {num_correct}/{total}")#\tps: {p_stats}\trs: {r_stats}")
+                    batch_err = 0
+                    num_correct, total = 0, 0
+                    p_stats = np.zeros((labels.shape[1]))
+                    r_stats = np.zeros((labels.shape[1]))
+
             
+            print(f"epoch acc: {epoch_acc} / {len(inputs)}\t({int((epoch_acc * 100)/len(inputs))}%)")
+            epoch_acc = 0
+            self.lr = LR
 
+def accuracy(pred, real):
+    num_correct = 0
+    total = len(pred)
+    for i in range(total):
+        if (np.argmax(pred[i]) == np.argmax(real[i])):
+            num_correct += 1
+    return num_correct, total
 
+def print_pred_real_acc(pred, real):
+    print("*************")
+    print("pred:")
+    print(pred)
+    print("\nreal:")
+    print(real)
+    print(f"acc: {accuracy(pred, real)}")
+    print("*************")
 
 def stats(preds, reals):
     p_stats = [0] * preds.shape[1]
@@ -114,13 +164,7 @@ def stats(preds, reals):
     return np.array(p_stats), np.array(r_stats)
 
             
-def accuracy(pred, real):
-    num_correct = 0
-    total = len(pred)
-    for i in range(total):
-        if (np.argmax(pred[i]) == np.argmax(real[i])):
-            num_correct += 1
-    return num_correct, total
+
 
 
 def get_error(pred, real):
@@ -138,3 +182,6 @@ def foo(v):
     else:
         return np.array([[1, 0]])
 
+def my_print(s):
+    if DEBUG:
+        print(s)

code/funcs.py
@@ -2,28 +2,32 @@ import numpy as np
 
 def softmax(xTw):
     #assumes x = xTranspose * w in the input
+    # e_x = np.exp(xTw - np.max(xTw))
     e_x = np.exp(xTw - np.max(xTw))
-    return e_x / np.sum(e_x, axis=1, keepdims=True)
-    # return e_x / e_x.sum()
+    output = e_x / np.sum(e_x, axis=1, keepdims=True)
+    return output
 
-    # x_transpose = x.T
-    # n = np.argmax(x_transpose.dot(w))
-
-    # denominator = np.dot(x_transpose, w) - n
-    # denominator = np.exp(denominator)
-    # denominator = denominator.sum()
-
-    # numerator = np.exp( np.dot(x_transpose, w[index]) - n)
-
-    # return numerator / denominator
 
 def cross_entropy(xTw, one_hot_vector):
     (n, m) = xTw.shape #TODO: handle edge cases
     return (-1/m) * np.sum(np.log(xTw) * one_hot_vector)
 
 def grad_softmax(s):
-    sm = softmax(s)
-    return sm * (np.ones(sm.shape) - sm)
+    func_print("****grad_softmax***")
+    func_print(f"s shape: {s.shape}")
+    SM = s.reshape((-1,1))
+    func_print(f"SM shape: {SM.shape}")
+    d = np.diagflat(s)
+    func_print(f"d shape: {d.shape}")
+    SM_dot_SMT = np.dot(SM, SM.T)
+    func_print(f"SM_dot_SMT shape: {SM_dot_SMT.shape}")
+    jac =  d - SM_dot_SMT
+    func_print(f"jac shape: {jac.shape}")
+    func_print(f"jac:\n {jac}")
+    #return jac
+    v = np.sum(jac, axis=0)
+    func_print(f"jac as vec: {v}")
+    return v
     # jacobian_m = np.diag(s)
 
     # for i in range(len(jacobian_m)):
@@ -35,13 +39,17 @@ def grad_softmax(s):
     # return jacobian_m
     
 
+DEBUG = False
+def func_print(s):
+    if DEBUG:
+        print(s)
+
 def tanh(x):
     return np.tanh(x)
 
 def tanh_tag(x):#todo: need to apply element by element
-    #print(f"tan` in: {x}")
     output = 1 - (np.tanh(x) * np.tanh(x))
-    #print(f"tan` out: {output}")
+    # output = x - (np.tanh(x) * np.tanh(x))
     return output
 
 def identity(x):
@@ -49,6 +57,12 @@ def identity(x):
 
 def identity_tag(x):
     return np.ones(x.shape)
+
+def relu(x):
+    return x * (x > 0)
+
+def relu_tag(x):
+    return 1. * (x > 0)
 
 
 def get_points(f, a, b, dx):
@@ -78,4 +92,3 @@ def print_softmax_points():
         file.close()
     
 
-print_softmax_points()

code/runner.py
@@ -1,12 +1,10 @@
+from utils import unison_shuffled_copies
 from mat_file_util import DataLoader
 import numpy as np
 import funcs as f
 from NN2 import NN
+from data_maker import make_swirl_data
 
-def unison_shuffled_copies(a, b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    return a[p], b[p]
 
 def argmax_data(dim=10, num_samples=25000):
     np.random.seed(42)
@@ -23,38 +21,71 @@ def argmax_data(dim=10, num_samples=25000):
 
     return inputs, labels
 
+def xor_data(dim=10, num_samples=25000):
+    np.random.seed(42)
+    inputs = np.random.randint(2, size=(num_samples, dim))
+    labels = np.random.rand(num_samples, 2)
+
+    for i in range(len(labels)):
+        c = int(np.sum(inputs[i]) % 2)
+        for j in range(2):
+            if j == c:
+                labels[i][j] = 1
+            else:
+                labels[i][j] = 0
+
+    return inputs, labels
     
 
 def main():
-    # path = '../data/PeaksData.mat'
-    path = '../data/SwissRollData.mat'
-    data_loader = DataLoader(path)
+    path = '../data/PeaksData.mat'
 
-    inputs = data_loader.training_inputs.T
-    labels = data_loader.training_labels.T
-    #inputs, labels = argmax_data(dim=10)
+    #path = '../data/SwissRollData.mat'
+    # data_loader = DataLoader(path)
 
-    in_dim = len(inputs[0])
-    num_labels = len(labels[0])
-    num_samples = len(inputs)
+    # inputs = data_loader.training_inputs.T
+    # labels = data_loader.training_labels.T
+    #inputs, labels = unison_shuffled_copies(inputs, labels)
+    inputs, labels = make_swirl_data(100)
+    # inputs = inputs[5:6]
+    # labels = labels[5:6]
 
+    in_dim, num_labels, num_samples = get_info(inputs, labels)
+
+    layer_dims = [in_dim, 100, num_labels]
+    print_info(in_dim, num_labels, num_samples, layer_dims)
+
+    nn = NN(layer_dims=layer_dims, act=f.tanh, act_tag=f.tanh_tag)
+    nn.train(inputs, labels, mini_batch_size=1, batch_size=100, num_epochs=10)
+
+
+
+
+
+
+def num_params(dims):
+    output = 1
+    for dim in dims:
+        output *= dim
+    return output
+
+def print_info(in_dim, num_labels, num_samples, layer_dims):
     print(f"in dim: {in_dim}")
     print(f"num labels: {num_labels}")
     print(f"num samples: {num_samples}")
+    print(f"num weights: {num_params(layer_dims)}")
 
-    # print(f"input shape: {inputs[0][None, :].shape}")
-    # print(f"output shape: {labels[0][None, :].shape}")
-    layer_dims = [in_dim, 5, 5, 10, 5, num_labels]
-    nn = NN(layer_dims=layer_dims, act=f.tanh, act_tag=f.tanh_tag)
+def get_info(inputs, labels):
+    in_dim = len(inputs[0])
+    num_labels = len(labels[0])
+    num_samples = len(inputs)
+    return in_dim, num_labels, num_samples
 
-    inputs, labels = unison_shuffled_copies(inputs, labels)
-    print(f"in shape: {inputs.shape}, labels shape: {labels.shape}")
-    # inputs = np.array(inputs)
-    # labels = np.array(labels)
-    nn.train(inputs, labels, mini_batch_size=1, batch_size=1)
-
-
-
+def print_swiss_points(inputs, labels, c):
+    for i in range(len(inputs)):
+        if labels[i][c] > -0.1:
+            input = inputs[i]
+            print(f"({input[0]}, {input[1]})")
 
 main()
 # my_data()